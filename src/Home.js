c6666f057285b911a5961852f17b2f76abe1dca3
kaungpyaesoneaunggic <kaungpyaesoneaung@gicjp.com>
2024-02-27 09:28:23 +0630

    Url updated

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import useFetch from "./useFetch";
 import React from 'react'
 
 export default function Home() {
-  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+  const { error, isPending, data: blogs } = useFetch('https://dojo-blog-xi.vercel.app/blogs/')
   return (
     
     <div className="home">
